9120a6a668d21f4b60e6ae90664f27f41ae125ba
capybara1004 <127846122+capybara1004@users.noreply.github.com>
2023-09-15 19:06:27 +0900

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void)
+{
+	int num1, num2;
+
+	printf("Input two integers: ");
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d & %d =%d \n", num1, num2, num1& num2);
+	printf("%d | %d =%d \n", num1, num2, num1 | num2);
+	printf("%d ^ %d =%d \n", num1, num2, num1^ num2);
+	return 0;
+
+}
